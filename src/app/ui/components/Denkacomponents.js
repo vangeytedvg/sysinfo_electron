8469030b8109a492c9cce72895fcf110492a9461
vangeytedvg@gmail.com <vangeytedvg@gmail.com>
2022-11-28 12:52:20 +0100

    Added custom component

---
+++ src/app/ui/components/Denkacomponents.js
@@ -0,0 +1,60 @@
+/**
+ * denkatitle.js
+ * The DenkaTech components library
+ */
+class DenkaTitle extends HTMLElement {
+    // internal component styling
+    static css = `
+        :host {            
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-weight: 700;
+            text-shadow: rgb(87, 126, 236) 1px 0 20px;
+            font-size: 2.5em;
+            text-align: center;
+            color: rgb(201, 201, 201);
+            margin: 50px;
+        }
+
+        .margins {
+            margin-top:50px;
+            margin-bottom:20px;
+        }
+    `;
+
+    static get observedAttributes() {
+        return ["title-text"];
+      }
+
+    // Always call the constructor for custom components
+    constructor() {
+        super();
+        // Activate the shadow DOM
+        this.attachShadow({mode: "open"});
+
+        // Set the style
+        const style = document.createElement("style");
+        style.innerHTML = DenkaTitle.css;
+        // Add the class
+        const titletext = document.createElement("div");
+        titletext.classList.add("title-text")
+        titletext.classList.add("margins")
+
+        // Get the caption attribute
+        titletext.innerText = this.my_caption;
+        
+        this.shadowRoot.append(style,titletext);
+    }
+
+    /* getters and setters */
+    get my_caption() {        
+        const capt = this.getAttribute("caption");
+        return capt;
+    }
+
+    set my_caption(value) {
+        this.setAttribute("title-text") = value ;
+      }
+
+}
+
+customElements.define("denka-title", DenkaTitle);
